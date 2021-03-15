af3586e106cbabd08186dcb0221beabc3af0c04a
antoniorestivo <nonebesides370@gmail.com>
2021-03-14 21:17:29 -0700

    Made sign in and sign up functional

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="signin">
     <h1>Sign In</h1>
-    <h2></h2>
+    <form v-on:submit.prevent="loginUser()">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }} </li>
+      </ul>
+      Email <input type="text" v-model="newEmail" />
+      password <input type="text" v-model="newPassword" />
+      <input type="submit" value="Submit" />
+    </form>
+
+    
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default{
+  data: function() {
+    return {
+      sameEmail: "",
+      samePassword: "",
+    };
+  },
+  created: function() {},
+  methods: {
+    loginUser: function() {
+      var params = {
+        email: this.sameEmail,
+        password: this.samePassword,
+      
+      };
+      axios
+      .post("/api/sessions",params)
+      .then(response => {
+        console.log("user login", response);
+        this.$router.push("/sessions")
+      })
+      .catch(error => {
+          console.log("user create error", error.response);
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,50 @@
 <template>
   <div class="signup">
     <h1>Sign Up</h1>
-    <h2></h2>
+    <form v-on:submit.prevent="createUser()">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }} </li>
+      </ul>
+      Email <input type="text" v-model="newEmail" />
+      password <input type="text" v-model="newPassword" />
+      confirm password <input type="text" v-model="newConfirmPassword" />
+      <input type="submit" value="Submit" />
+    </form>
+
+    
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default{
+  data: function() {
+    return {
+      newEmail: "",
+      newPassword: "",
+      newConfirmPassword: ""
+    };
+  },
+  created: function() {},
+  methods: {
+    createUser: function() {
+      var params = {
+        email: this.newEmail,
+        password: this.newPassword,
+        confirm_password: this.newConfirmPassword
+      };
+      axios
+      .post("/api/users",params)
+      .then(response => {
+        console.log("user create", response);
+        this.$router.push("/users")
+      })
+      .catch(error => {
+          console.log("user create error", error.response);
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
